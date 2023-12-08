39494d03c0a5f5e6bbff1212c7629713a99ad20c
nmusale <nikhilmusale70@gmail.com>
2023-12-08 11:56:25 +0530

    Done with 7 dec leet-code, question was about to find the largest odd number substring

--- LeetCode/Temp.java
+++ LeetCode/Temp.java
@@ -1,5 +1,25 @@
+import java.util.HashMap;
+
 public class Temp {
     public static void main(String[] args) {
-        String str = "nikhil";
+        System.out.println("ehlloo");
+        Temp obj = new Temp();
+        int[] arr = {24,22,46,40,26,28,10,47,4,3,26,26};
+        System.out.println(obj.getPairsCount(arr, arr.length, 50));
+    }
+    //   int[] arr = {24,22,46,40,26,28,10,47,26,26,4,3};
+    int getPairsCount(int[] arr, int n, int k) {
+
+        int count = 0;
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        hash.put(26, 1);
+        for(int i=n-1;i>=0;i--){
+            if(hash.containsKey(k-arr[i])) {
+                count += hash.get(k-arr[i]);
+            }
+
+            hash.put(arr[i], hash.getOrDefault(hash.get(arr[i]), 1) + 1);
+        }
+        return count;
     }
 }
