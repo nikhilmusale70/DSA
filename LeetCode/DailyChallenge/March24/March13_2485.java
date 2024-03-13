2c16adb89b15140031d09684481bf34cc699cfcc
nmusale <nikhilmusale70@gmail.com>
2024-03-13 07:55:34 +0530

    March 13 LeetCode daily question, find pivot element

--- LeetCode/DailyChallenge/March24/March13_2485.java
+++ LeetCode/DailyChallenge/March24/March13_2485.java
@@ -9,9 +9,9 @@ public class March13_2485 {
 
 
     public int pivotInteger(int n) {
-        int y = n*(n+1)/2;
-        int x = (int)Math.sqrt(y);
-        if(x*x==y) return x;
+        int sum = n*(n+1)/2;
+        int pivot = (int)Math.sqrt(sum);
+        if(pivot*pivot==sum) return pivot;
         else return -1;
     }
 
