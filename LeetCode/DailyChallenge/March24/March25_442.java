be3bf5af8fccc91eca5a4d9402b5b48823672029
Nikhil Musale <43263589+nikhilmusale70@users.noreply.github.com>
2024-03-25 09:31:46 +0530

    LeetCode march 25, find all duplicate elements

---
+++ LeetCode/DailyChallenge/March24/March25_442.java
@@ -0,0 +1,25 @@
+package DailyChallenge.March24;
+
+public class March25_442{
+	public List<Integer> findDuplicates(int[] nums) {
+		List<Integer> result = new ArrayList<>();
+		int n = nums.length;
+
+		for (int i = 0; i < n; i++) {
+			// Take the absolute value of the current number
+			int num = Math.abs(nums[i]);
+			// Calculate the index based on the value of the number
+			int idx = num - 1;
+
+			// If the number at the calculated index is negative,
+			// it means we have encountered this number before
+			if (nums[idx] < 0)
+				result.add(num);
+
+			// Mark the number at the calculated index as visited
+			nums[idx] *= -1;
+		}
+
+		return result;
+	}
+}
