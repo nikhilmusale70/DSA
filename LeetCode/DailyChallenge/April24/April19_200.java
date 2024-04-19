797577dd9cc6fda42dfb8c7e7960a8c5439e8f3a
nmusale <nikhilmusale70@gmail.com>
2024-04-19 09:49:55 +0530

    April 19 leetcode daily question, find number of island problem

---
+++ LeetCode/DailyChallenge/April24/April19_200.java
@@ -0,0 +1,46 @@
+package DailyChallenge.April24;
+
+public class April19_200 {
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}};
+        April19_200 obj = new April19_200();
+
+        System.out.println(obj.numIslands(grid));
+    }
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] == '1') {
+                    count++;
+                    dfs(grid, row, col);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
+        }
+
+        grid[row][col] = '0';
+
+        dfs(grid, row - 1, col); // up
+        dfs(grid, row + 1, col); // down
+        dfs(grid, row, col - 1); // left
+        dfs(grid, row, col + 1); // right
+    }
+
+
+}
