917fc037cfa79cb76b49f143c91c615ef239fa4d
nmusale <nikhilmusale70@gmail.com>
2024-04-20 09:21:10 +0530

    April 20 leetcode daily question, find number of farmlands, linear greedy algo

--- LeetCode/DailyChallenge/April24/April20_1992.java
+++ LeetCode/DailyChallenge/April24/April20_1992.java
@@ -1,74 +1,42 @@
 package DailyChallenge.April24;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class April20_1992 {
     public static void main(String[] args) {
         April20_1992 obj = new April20_1992();
-        int[][] land = {{1,0,0},{0,1,1},{0,1,1}};
+        int[][] land = {
+                {1,0,0},
+                {0,1,1},
+                {0,1,1}};
         System.out.println(Arrays.deepToString(obj.findFarmland(land)));
     }
-    class Pair{
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
     public int[][] findFarmland(int[][] land) {
-        ArrayList<int[]> ans = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
 
-        for(int i=0; i<land.length; i++){
-            for(int j=0; j<land[i].length; j++){
-                if(land[i][j] == 1){
-                    //farmLand
-                    Pair pr = bfs(land, i, j);
-                    ans.add(new int[]{i,j, pr.x, pr.y});
-                    i=pr.x;
-                    j=pr.y;
+        for (int i = 0; i < land.length; i++) {
+            for (int j = 0; j < land[i].length; j++) {
+                if (land[i][j] == 1) {
+                    int[] landCoordinates = findLandBoundary(land, i, j);
+                    ans.add(landCoordinates);
                 }
             }
         }
-        int[][] answer = new int[ans.size()][4];
-        int i=0;
-        for (int[] arr: ans){
-            answer[i] = arr;
-            i++;
-        }
-        return answer;
+
+        return ans.toArray(new int[0][]);
     }
-    int[] xx = {-1, 0, 0, 1};
-    int[] yy = {0, -1, 1, 0};
-    Pair bfs(int[][] land, int x, int y){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(x, y));
-        Pair pr = q.peek();
-        while(!q.isEmpty()){
-            pr = q.remove();
-            land[pr.x][pr.y] = 0;
-            for(int i=0; i<4; i++){
-                int newX = pr.x + xx[i];
-                int newY = pr.y + yy[i];
-                if(isValid(land, newX, newY)){
-                    q.add(new Pair(newX, newY));
-                }
+
+    private int[] findLandBoundary(int[][] land, int startX, int startY) {
+        int endX = startX;
+        int endY = startY;
+
+        for (; endX < land.length && land[endX][startY] == 1; endX++) {
+            for (endY = startY; endY < land[0].length && land[endX][endY] == 1; endY++) {
+                land[endX][endY] = 0;
             }
         }
-        //we have right most index in pr
-        return pr;
-    }
 
-    boolean isValid(int[][] land, int x, int y){
-        if(x<0 || y<0 || x>=land.length || y>=land[0].length ){
-            return false;
-        }
-        return land[x][y]==1;
+        return new int[]{startX, startY, endX - 1, endY - 1};
     }
-
 }
