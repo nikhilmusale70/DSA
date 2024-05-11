c640f2ac7d38be6b86aaadb32960e51a55568060
nikhil <nikhilmusale70@gmail.com>
2024-05-11 13:58:33 +0530

    may 11 leetcode daily question, get the min cost to hire k workers, solution with priority queue

--- LeetCode/DailyChallenge/May24/May11_857.java
+++ LeetCode/DailyChallenge/May24/May11_857.java
@@ -1,43 +1,56 @@
 package DailyChallenge.May24;
 
+import Structural.Decorator.PizzaDecorator;
+
+import java.util.*;
+
 public class May11_857 {
     public static void main(String[] args) {
-        int[] quality = {10,20,5};
-        int [] wage = {70,50,30};
-        int k = 2;
+        int[] quality = {3,1,10,10,1};
+        int [] wage = {4,8,2,2,7};
+        int k = 3;
         May11_857 obj = new May11_857();
         System.out.println(obj.mincostToHireWorkers(quality, wage, k));
     }
+    public static class Pair{
+        double ratio;
+        int idx;
+
+        public Pair(double ratio, int idx) {
+            this.ratio = ratio;
+            this.idx = idx;
+        }
+    }
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        double ans = Double.MAX_VALUE;
-        for(int i = 0; i < quality.length; i++) {
-            ans = Math.min(ans, wage[i] + helper(quality, wage, k - 1, 0, i, (double) wage[i] / quality[i], new Double[quality.length][k + 1]));
+
+        List<Pair> workers = new ArrayList<>();
+        for(int i=0; i<wage.length; i++){
+            workers.add(new Pair((double) wage[i] /quality[i], i));
+        }
+        workers.sort(Comparator.comparingDouble(a -> a.ratio));
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int team = 0;
+        for (int i=0; i<k; i++){
+            int currWorkerQuality = quality[workers.get(i).idx];
+            maxHeap.add(currWorkerQuality);
+            team += currWorkerQuality;
+        }
+        double ans = team*workers.get(k-1).ratio;
+
+        for(int lead=k; lead< wage.length; lead++){
+            double leadsRatio = workers.get(lead).ratio;
+            int currWorkerQuality = quality[workers.get(lead).idx];
+
+            if (!maxHeap.isEmpty() && maxHeap.peek()>currWorkerQuality){
+                team = team - maxHeap.peek() + currWorkerQuality;
+                maxHeap.remove();
+                maxHeap.add(currWorkerQuality);
+            }
+
+            double captainTeamsRatio = leadsRatio * team;
+            ans = Math.min(ans, captainTeamsRatio);
         }
         return ans;
-    }
-
-    double helper(int[] quality, int[] wage, int k, int idx, int selected, double ratio, Double[][] memo) {
-        if (k == 0) {
-            return 0;
-        }
-        if (idx == quality.length) {
-            return Integer.MAX_VALUE;
-        }
-        if (memo[idx][k] != null) {
-            return memo[idx][k];
-        }
-
-        double take = Double.MAX_VALUE;
-        if (idx != selected && quality[idx] * ratio >= wage[idx]) {
-            take = quality[idx] * ratio + helper(quality, wage, k - 1, idx + 1, selected, ratio, memo);
-        }
-        double dontTake = helper(quality, wage, k, idx + 1, selected, ratio, memo);
-
-        double result = Math.min(take, dontTake);
-
-        // Memoize the result
-        memo[idx][k] = result;
-
-        return result;
     }
 }
