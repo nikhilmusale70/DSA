e34ffab39902973f12bae5592c1542f1caa048b4
nikhil <nikhilmusale70@gmail.com>
2024-05-11 09:35:05 +0530

    may 11 leetcode daily question, get the min cost to hire k workers, Recursive soltuion with DP but still will give you TLE

--- LeetCode/DailyChallenge/May24/May11_857.java
+++ LeetCode/DailyChallenge/May24/May11_857.java
@@ -10,27 +10,34 @@ public class May11_857 {
     }
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
         double ans = Double.MAX_VALUE;
-        for(int i=0; i<quality.length; i++){
-            ans = Math.min(ans, wage[i] + helper(quality, wage, k-1, i+1, (double) wage[i] /quality[i]));
+        for(int i = 0; i < quality.length; i++) {
+            ans = Math.min(ans, wage[i] + helper(quality, wage, k - 1, 0, i, (double) wage[i] / quality[i], new Double[quality.length][k + 1]));
         }
         return ans;
     }
 
-    double helper(int[] quality, int[] wage, int k, int idx, double ratio){
+    double helper(int[] quality, int[] wage, int k, int idx, int selected, double ratio, Double[][] memo) {
         if (k == 0) {
             return 0;
         }
-        if(idx == quality.length){
+        if (idx == quality.length) {
             return Integer.MAX_VALUE;
+        }
+        if (memo[idx][k] != null) {
+            return memo[idx][k];
         }
 
         double take = Double.MAX_VALUE;
-        if(quality[idx]*ratio>=wage[idx]){
-            //only then we can hire
-            take = quality[idx]*ratio + helper(quality, wage, k-1, idx+1, ratio);
+        if (idx != selected && quality[idx] * ratio >= wage[idx]) {
+            take = quality[idx] * ratio + helper(quality, wage, k - 1, idx + 1, selected, ratio, memo);
         }
-        double dontTake = helper(quality, wage, k, idx+1, ratio);
+        double dontTake = helper(quality, wage, k, idx + 1, selected, ratio, memo);
 
-        return Math.min(take, dontTake);
+        double result = Math.min(take, dontTake);
+
+        // Memoize the result
+        memo[idx][k] = result;
+
+        return result;
     }
 }
