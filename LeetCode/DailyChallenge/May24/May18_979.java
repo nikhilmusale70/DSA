8407ba7936eec3de9a79a45a427d876a47f65721
nikhil <nikhilmusale70@gmail.com>
2024-05-18 12:36:43 +0530

    May 18 LeetCode daily question, distribute coins in binary tree

---
+++ LeetCode/DailyChallenge/May24/May18_979.java
@@ -0,0 +1,36 @@
+package DailyChallenge.May24;
+
+public class May18_979 {
+    class Solution {
+        int moves;
+        public int distributeCoins(TreeNode root) {
+            moves = 0;
+            helper(root);
+            return moves;
+
+        }
+        int helper(TreeNode root){
+            if(root==null) return 0;
+
+            int left = helper(root.left);
+            int right = helper(root.right);
+
+            moves += Math.abs(left) + Math.abs(right);
+
+            return (root.val -1) + left + right;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        May17_1325.TreeNode left;
+        May17_1325.TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, May17_1325.TreeNode left, May17_1325.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
