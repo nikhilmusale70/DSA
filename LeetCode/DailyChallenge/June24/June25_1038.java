6569a27ba2d0179a41fff1560ec5286897f0b711
nikhil <nikhilmusale70@gmail.com>
2024-06-25 10:18:18 +0530

    June 25 Leetcode daily question, do some operations on bst

---
+++ LeetCode/DailyChallenge/June24/June25_1038.java
@@ -0,0 +1,149 @@
+package DailyChallenge.June24;
+
+import java.util.*;
+
+public class June25_1038 {
+
+
+    public static void main(String[] args) {
+        June25_1038 obj = new June25_1038();
+        Integer[] arr = {4,1,6,0,2,5,7,null,null,null,3,null,null,null,8};
+        TreeNode root = createTree(arr);
+        printTree(root);
+        System.out.println();
+        printTree(obj.bstToGst(root));
+    }
+
+    public TreeNode bstToGst( TreeNode root) {
+        traversal(root);
+        return root;
+    }
+
+    int parent=0;
+    void traversal(TreeNode root){
+        if(root==null) return;
+        traversal(root.right);
+        root.val = root.val+parent;
+        parent = Math.max(root.val, parent);
+        traversal(root.left);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode createTree(Integer[] arr) {
+        if (arr.length == 0) return null;
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (i < arr.length) {
+            TreeNode current = queue.poll();
+            if (arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            System.out.println("Tree is empty");
+            return;
+        }
+
+        int maxLevel = maxLevel(root);
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static void printNodeInternal(List<TreeNode> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        printWhitespaces(firstSpaces);
+
+        List<TreeNode> newNodes = new ArrayList<>();
+        for (TreeNode node : nodes) {
+            if (node != null) {
+                System.out.print(node.val);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static int maxLevel(TreeNode node) {
+        if (node == null)
+            return 0;
+        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
+
+    private static boolean isAllElementsNull(List<TreeNode> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+        return true;
+    }
+}
