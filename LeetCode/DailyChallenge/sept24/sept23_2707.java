2cfddfb206ab3d8f455b256d5626edbf3cf68194
nikhil <nikhilmusale70@gmail.com>
2024-09-23 11:28:02 +0530

    sept 23 Leetcode daily, find extra chars with memoization

--- LeetCode/DailyChallenge/sept24/sept23_2707.java
+++ LeetCode/DailyChallenge/sept24/sept23_2707.java
@@ -11,13 +11,17 @@ public class sept23_2707 {
         sept23_2707 obj = new sept23_2707();
         System.out.println(obj.minExtraChar(s, dictionary));
     }
+    Integer[] dp;
     public int minExtraChar(String s, String[] dictionary) {
+        dp = new Integer[s.length()];
         Set<String> set = new HashSet<>(Arrays.asList(dictionary));
         return helper(0, s.length(), s, set);
     }
 
     int helper(int s, int e, String str, Set<String> dictionary){
         if(s==e) return 0;
+        if(dp[s] != null) return dp[s];
+
         //skip the char
         int ans = 0;
         ans = helper(s+1, e, str, dictionary) + 1;
@@ -28,6 +32,6 @@ public class sept23_2707 {
                 ans = Math.min(ans, helper(idx+1, e, str, dictionary));
             }
         }
-        return ans;
+        return dp[s] = ans;
     }
 }
