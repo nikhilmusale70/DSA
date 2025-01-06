bb45feed523251d6f7adb80f631143f4908fb32b
nikhil <nikhilmusale70@gmail.com>
2025-01-06 11:57:30 +0530

    jan6 Leetcode daily, min operations to move all balls, optimised

--- LeetCode/DailyChallenge/Jan25/jan6.java
+++ LeetCode/DailyChallenge/Jan25/jan6.java
@@ -12,15 +12,25 @@ public class jan6 {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
         int[] ans = new int[n];
+
+        //left to right
+        int move = 0;
+        int balls = 0;
         for(int i=0; i<n; i++){
-            int moves = 0;
-            for(int j=0; j<n; j++){
-                if(i==j) continue;
-                if(boxes.charAt(j)=='0') continue;
-                moves += Math.abs(i-j);
-            }
-            ans[i] = moves;
+            ans[i] = move + balls;
+            move += balls;
+            if(boxes.charAt(i)=='1') balls++;
         }
+
+        //right to left
+        move = 0;
+        balls = 0;
+        for(int i=n-1; i>=0; i--){
+            ans[i] += move + balls;
+            move += balls;
+            if(boxes.charAt(i)=='1') balls++;
+        }
+
         return ans;
     }
 }
